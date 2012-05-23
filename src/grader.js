4bdd4bd7566da098eb676ef7051bcdf37ef74bad
Peter Walker <peterw@suprafax.net>
2012-05-22 20:16:30 -0500

    initial sketches
    still flushing out the details.

---
+++ src/grader.js
@@ -0,0 +1,13 @@
+/*
+
+a provider will provide endless (almost) chunks of text!  It is essentially an interator.
+
+boolean provider.hasNext();
+string provider.next();
+
+*/
+var grader = function (corpusProvider, candidateProvider) {
+	
+
+
+};
---
+++ src/nqueen/nqueen.js
@@ -0,0 +1,16 @@
+var nqueen = function (size, candidateProvider, options) {
+
+/*
+
+	// ensure object is of the right format!
+	boolean validate(candidate);
+
+	// mutate the right amount of the candidate, possibly paying attention to the options
+	void mutate(candidate);
+
+	// mixup the two candidates, possibly paying attention to options
+	void crossover(candidate1, candidate2);
+
+*/
+
+};
